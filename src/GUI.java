fb31587a69b139eb9fda69109178b3184f4cc457
2042426 Jhon Alexander Valencia Hilamo <jhon.hilamo@correounivalle.edu.co>
2022-03-02 22:36:48 -0500

    Creating Interface

--- src/GUI.java
+++ src/GUI.java
@@ -1,14 +1,22 @@
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * This class is used for ...
  * @author Jhon Alexander Valencia Hilamo jhon.hilamo@correounivalle.edu.co
+ * @author Janiert Sebastian Salas Castillo janiert.salas@correounivalle.edu.co
  * @version v.1.0.0 date:21/11/2021
  */
 public class GUI extends JFrame {
-
+  GridBagConstraints constraints = new GridBagConstraints();
   private Header headerProject;
+  private JPanel panelBarcos,panelMiTablero,panelTableroPc,panelTableroPc2,base,enemigo;
+  private Escucha escucha;
+  private JButton ayuda,salir,tableroPc,quitarTablero;
 
   /**
    * Constructor of GUI class
@@ -18,8 +26,10 @@ public class GUI extends JFrame {
 
     //Default JFrame configuration
     this.setTitle("The Title app");
-    this.setSize(200,100);
-    //this.pack();
+    //this.setPreferredSize(new Dimension(1095,528));
+    this.setBackground(new Color(255,255,255));
+    this.setUndecorated(true);
+    this.pack();
     this.setResizable(true);
     this.setVisible(true);
     this.setLocationRelativeTo(null);
@@ -31,12 +41,85 @@ public class GUI extends JFrame {
    * create Listener and control Objects used for the GUI class
    */
   private void initGUI() {
-    //Set up JFrame Container's Layout
-    //Create Listener Object and Control Object
-    //Set up JComponents
-    headerProject = new Header("Header ...", Color.BLACK);
+    this.getContentPane().setLayout(new GridBagLayout());
 
-    this.add(headerProject,BorderLayout.NORTH); //Change this line if you change JFrame Container's Layout
+
+    escucha =new Escucha();
+
+    headerProject = new Header("Batalla Naval", Color.BLACK);
+    constraints.gridx=0;
+    constraints.gridy=0;
+    constraints.gridwidth=4;
+    constraints.fill=GridBagConstraints.HORIZONTAL;
+    this.add(headerProject,constraints);
+
+    ayuda = new JButton(" ? ");
+    ayuda.addActionListener(escucha);
+    constraints.gridx=0;
+    constraints.gridy=1;
+    constraints.gridwidth=1;
+    constraints.fill=GridBagConstraints.NONE;
+    constraints.anchor=GridBagConstraints.LINE_START;
+    this.add(ayuda,constraints);
+
+    salir = new JButton(" X ");
+    salir.addActionListener(escucha);
+    constraints.gridx=2;
+    constraints.gridy=1;
+    constraints.gridwidth=1;
+    constraints.fill=GridBagConstraints.NONE;
+    constraints.anchor=GridBagConstraints.LINE_END;
+    this.add(salir,constraints);
+
+    panelBarcos=new JPanel();
+    panelBarcos.setPreferredSize(new Dimension(165, 315));
+    panelBarcos.setBorder(BorderFactory.createTitledBorder(new LineBorder(new Color(0,0,0),2,true),"Mis Barcos", TitledBorder.CENTER,TitledBorder.TOP,new Font("Tahoma", 1, 14)));
+    constraints.gridx=0;
+    constraints.gridy=2;
+    constraints.gridwidth=1;
+    //constraints.gridheight=1;
+    constraints.fill=GridBagConstraints.NONE;
+    constraints.anchor=GridBagConstraints.PAGE_START;
+    add(panelBarcos,constraints);
+
+    tableroPc = new JButton("Mostrar Tablero");
+    tableroPc.addActionListener(escucha);
+    constraints.gridx=2;
+    constraints.gridy=3;
+    constraints.gridwidth=1;
+    constraints.fill=GridBagConstraints.NONE;
+    constraints.anchor=GridBagConstraints.FIRST_LINE_START;
+    this.add(tableroPc,constraints);
+
+    quitarTablero = new JButton("Ocultar Tablero");
+    quitarTablero.addActionListener(escucha);
+    constraints.gridx=2;
+    constraints.gridy=3;
+    constraints.gridwidth=1;
+    constraints.fill=GridBagConstraints.NONE;
+    constraints.anchor=GridBagConstraints.FIRST_LINE_END;
+    this.add(quitarTablero,constraints);
+
+    panelMiTablero=new JPanel();
+    panelMiTablero.setPreferredSize(new Dimension(440, 400));
+    panelMiTablero.setBorder(BorderFactory.createTitledBorder(new LineBorder(new Color(0, 0, 0),3,true),"BASE",TitledBorder.CENTER,TitledBorder.TOP,new Font("Tahoma", 1, 15)));
+    constraints.gridx=1;
+    constraints.gridy=2;
+    constraints.gridwidth=1;
+    constraints.fill=GridBagConstraints.NONE;
+    constraints.anchor=GridBagConstraints.CENTER;
+    add(panelMiTablero,constraints);
+
+    panelTableroPc=new JPanel();
+    panelTableroPc.setPreferredSize(new Dimension(440, 400));
+    panelTableroPc.setBorder(BorderFactory.createTitledBorder(new LineBorder(new Color(0, 0, 0),3,true),"MAPA",TitledBorder.CENTER,TitledBorder.TOP,new Font("Tahoma", 1, 15)));
+    constraints.gridx=2;
+    constraints.gridy=2;
+    constraints.gridwidth=1;
+    constraints.fill=GridBagConstraints.NONE;
+    constraints.anchor=GridBagConstraints.CENTER;
+    add(panelTableroPc,constraints);
+
   }
 
   /**
@@ -53,7 +136,26 @@ public class GUI extends JFrame {
   /**
    * inner class that extends an Adapter Class or implements Listeners used by GUI class
    */
-  private class Escucha {
+  private class Escucha implements ActionListener {
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      if (e.getSource()==salir){
+        System.exit(0);
+      }
+
+      if(e.getSource()==tableroPc){
+        JOptionPane.showMessageDialog(null,"Mostrar");
+        panelTableroPc2=new JPanel();
+        panelTableroPc2.setPreferredSize(new Dimension(440, 448));
+        panelTableroPc2.setBorder(BorderFactory.createTitledBorder(new LineBorder(new Color(0, 0, 0),3,true),"MAPA",TitledBorder.CENTER,TitledBorder.TOP,new Font("Tahoma", 1, 15)));
+        constraints.gridx=1;
+        constraints.gridy=3;
+        constraints.gridwidth=1;
+        constraints.fill=GridBagConstraints.NONE;
+        constraints.anchor=GridBagConstraints.CENTER;
+        add(panelTableroPc2,constraints);
+      }
+    }
   }
 }
